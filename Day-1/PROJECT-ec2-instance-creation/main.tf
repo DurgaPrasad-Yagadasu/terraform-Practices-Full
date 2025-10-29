ea7edd7d93516126c6f1d9c9917f0b39fa10a61b
DurgaPrasad-Yagadasu <66588061+DurgaPrasad-Yagadasu@users.noreply.github.com>
2025-10-29 12:51:23 +0530

    Modify EC2 instance type and add availability zones
    Updated instance type and added availability zone configuration.

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,7 +2,15 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
+# Fetch all available Availability Zones
+data "aws_availability_zones" "available" {
+    state = "available"
+}
+
 resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
+    subnet_id     = "subnet-00d448c4c9d49cc94" # Specify an appropriate SUBENT ID
+    key_name      = "My_Key" # Specify an appropriate KEY Name
+    availability_zone = data.aws_availability_zones.available.names[0]
 }
